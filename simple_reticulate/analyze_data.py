238bc14ad882c3ecbd1d01341faf7d762317cb4e
taiawu <taiawu@gmail.com>
2020-10-29 18:23:54 -0700

    output probabilities as dataframe, minor GUI changes

--- simple_reticulate/analyze_data.py
+++ simple_reticulate/analyze_data.py
@@ -27,7 +27,7 @@ def classify(model_path, rdf):
     return df
 
 def format_data(rdf):
-    data_array = [np.array(rdf[k]) for k in rdf.keys() if k != 'well']
+    data_array = [np.array(rdf[k]) for k in sorted(rdf.keys()) if k != 'well']
     data_array = np.vstack(data_array)
     wells = np.array(rdf['well'])
     data_array = data_array.T
--- simple_reticulate/app.R
+++ simple_reticulate/app.R
@@ -21,8 +21,10 @@ server <- function(input, output) {
     # set up the reactive values container 
     values <- reactiveValues()
     
+    #values$data_name <- "Exp0618--OLF-processed.rds"
+    
     # read in sample formatted data
-    values$df_all <- readRDS("sample_data_three_dyes.rds") 
+    values$df_all <- readRDS("20201029_olf_reprocessed.rds") 
     
 ### -------------- Condition data for neural net -------------- ###
     # triggers when values$df_all appears or changes
@@ -47,7 +49,22 @@ server <- function(input, output) {
       values$df_outcomes <- values$python_output %>% 
                               set_names(c("reject", "sensitive", "hit", "well")) %>%
                               pivot_longer(-well, names_to = "outcome", values_to = "log_prob") %>%
-                              left_join( ., values$df_all %>% select(well, dye) %>% distinct(), by = "well")
+                              left_join( ., values$df_all %>% select(well, dye) %>% distinct(), by = "well") %>%
+                              filter(dye != "Empty")
+      
+      values$df_outcomes_summary <- values$df_outcomes %>%
+                                    group_by(well) %>%
+                                    filter(log_prob == max(log_prob))
+      
+      
+      values$df_outcomes_tally <- values$df_outcomes %>%
+                                    group_by(well) %>%
+                                    filter(log_prob == max(log_prob)) %>%
+                                    ungroup() %>%
+                                    count(outcome)
+      
+      values$hits <- values$df_outcomes_summary %>% filter(outcome == "hit") %>% arrange(dye) %>% pull(dye)
+      values$sensitive <- values$df_outcomes_summary %>% filter(outcome == "sensitive")  %>% arrange(dye) %>% pull(dye)
       
       print(values$python_output %>% str()) 
     })
@@ -55,17 +72,23 @@ server <- function(input, output) {
 
     
 ### -------------- Render GUI elements -------------- ###
-    output$plot <- renderPlot( print(facet_wrap_linetyped2(values$df_all, title = "Plot test!", facets_wide = 6 ))) 
+   # output$plot <- renderPlot( print(facet_wrap_linetyped2(values$df_all, title = "Plot test!", facets_wide = 6 ))) 
     output$which_python <- renderText({  values$python_output })
-    output$raw_table <- renderTable({ values$df_wide })
-    output$prob_table <- renderTable({ values$df_outcomes })
+    #output$raw_table <- renderTable({ values$df_wide %>% head() })
+    output$prob_table <- renderTable({ values$df_outcomes_summary %>% head() })
+    output$prob_table_tally <- renderTable({ values$df_outcomes_tally })
+    output$hit_list <- renderText( values$hits)
+    output$sensitive_list <- renderText(values$sensitive)
 }
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(useShinyalert(),
                          # plotOutput("plot"), # display plot
-                          tableOutput('raw_table'), # print df_wide, the data passed to the NN
-                tableOutput('prob_table')
+                         # tableOutput('raw_table'), # print df_wide, the data passed to the NN
+                tableOutput('prob_table'),
+                tableOutput('prob_table_tally'),
+                verbatimTextOutput('hit_list'),
+                verbatimTextOutput('sensitive_list')
 )
 
 shinyApp(ui, server) # run the app
